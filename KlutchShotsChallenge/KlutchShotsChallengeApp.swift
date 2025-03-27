660a6a6b6145ae3780741b142e97cbdeb16ae8d5
marcosiracusano <marc.siracusano@gmail.com>
2025-03-27 19:25:32 -0300

    Initial Commit

---
+++ KlutchShotsChallenge/KlutchShotsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlutchShotsChallengeApp.swift
+//  KlutchShotsChallenge
+//
+//  Created by Marco Siracusano on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct KlutchShotsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
